a712eb5f3dd682203424b20dfb59cf12194ce794
valentino <126835108+Valen89hh@users.noreply.github.com>
2024-05-15 07:34:30 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import http from "http"
+import { fileURLToPath } from 'url'
+import {dirname, join} from "node:path"
+import { Server } from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname)
+app.get('/', (req, res) => res.sendFile(join(__dirname, "index.html")))
+
+
+
+
+const PORT = 3000
+server.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`))
