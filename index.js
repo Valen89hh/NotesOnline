140b46de05d15a4d1635411fb0be0d88cd2f9339
valentino <126835108+Valen89hh@users.noreply.github.com>
2024-06-05 14:41:30 -0500

    add routes

--- index.js
+++ index.js
@@ -3,35 +3,73 @@ import http from "http"
 import { fileURLToPath } from 'url'
 import {dirname, join} from "node:path"
 import { Server } from 'socket.io'
+import cryptoRandomString from "crypto-random-string"
+import router from './routes/index.js'
+
 
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
+const db = [
+    {
+        server: "",
+        userId: ""
+    }
+]
+
 const __dirname = dirname(fileURLToPath(import.meta.url))
 app.use(express.static(join(__dirname, 'public')));
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'public', 'index.html'));
-});
+app.use('/', router);
 
 io.on("connection", (socket)=>{
     console.log("User connect ✅")
 
+    socket.on("createServer", ()=>{
+        const newServerName = cryptoRandomString({length: 10})
+        console.log(newServerName)
+        socket.emit("statusServer", newServerName)
+        socket.join(newServerName)
+        db.push({
+            server: newServerName,
+            userId: socket.id
+        })
+    })
+
 
     socket.on("join server", (nameServer)=>{
-        console.log(nameServer)
-        socket.join(nameServer)
-        io.to(nameServer).emit("h1", `User Conected servidor: ${nameServer}`)
+        const userServer = db.find(ser=>ser.server == nameServer)
+        if(userServer){
+            socket.join(userServer.server)
+            io.to(userServer.server).emit("h1", `User Conected servidor: ${nameServer}`)
+            io.to(userServer.userId).emit("solicitarNotas", socket.id)
+        }else{
+            console.log("Server not found")
+        }
+    })
+
+    socket.on("enviarNotas", (idReceptor, notas)=>{
+        io.to(idReceptor).emit("notasIniciales", notas)
     })
 
     socket.on("leave server", (nameServer)=>{
         console.log(nameServer)
-        io.to(nameServer).emit("h1", `User Desconectado del servidor: ${nameServer}`)
-        socket.leave(nameServer)
+        const room = db.find(sev=>sev.userId == socket.id)
+        if(room){
+            console.log("Salir All rooms")
+            io.socketsLeave(nameServer)
+        }else{
+            io.to(nameServer).emit("h1", `User Desconectado del servidor: ${nameServer}`)
+            socket.leave(nameServer)
+        }
     })
 
-    socket.on("update note", (nameServer,noteData)=>{
-        io.to(nameServer).emit("update note", noteData)
+    socket.on('nuevaNota', (nombreSala, nuevaNota) => {
+        socket.broadcast.to(nombreSala).emit('nuevaNota', nuevaNota);
+    });
+
+    socket.on("moverNota", (nameServer, notaId, notaPosition)=>{
+        io.to(nameServer).emit("moverNota", notaId, notaPosition)
     })
 
     socket.on("disconnect", ()=> console.log("User disconnect ❌"))
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const router = Router();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Ruta para el Home
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Ruta para About
+router.get('/join', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/join.html'));
+});
+
+// Ruta para Contact
+router.get('/server/:id', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/server.html'));
+});
+
+export default router;
